c588b72eafa66939d9db95f5e7477671694d9f62
Andrewschaer <andrewschaer@github.com>
2021-09-09 22:50:46 -0400

    built server and routers foundation in index.js & server.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// start your server here
+const server = require('./api/server.js');
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
